fabf09cf90880dd028d5f477a12515f5a6747096
idarciooliveira <idarciooliveira@gmail.com>
2023-08-29 00:09:47 +0100

    day01

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
